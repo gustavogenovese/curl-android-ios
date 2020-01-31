c69f452b2585cb55c8b4cb871ee69692002b210d
Morten Daniel Fornes <mdfornes@gmail.com>
2020-01-31 14:51:14 +0700

    Android: update list of source files

--- curl-compile-scripts/jni/curl.mk
+++ curl-compile-scripts/jni/curl.mk
@@ -12,28 +12,29 @@ CURL_COMMON_CFLAGS += \
   -Wno-system-headers -Wno-typedef-redefinition -Wno-unused-variable \
   -Wno-unused-function
 CURL_CSOURCES := \
-  amigaos.c asyn-ares.c asyn-thread.c base64.c conncache.c \
-  connect.c content_encoding.c cookie.c curl_addrinfo.c curl_ctype.c curl_des.c \
-  curl_endian.c curl_fnmatch.c curl_gethostname.c curl_gssapi.c curl_memrchr.c \
-  curl_multibyte.c curl_ntlm_core.c curl_ntlm_wb.c curl_path.c curl_range.c \
-	curl_rtmp.c curl_sasl.c curl_sspi.c curl_threads.c dict.c dotdot.c easy.c \
-	escape.c file.c fileinfo.c formdata.c ftp.c ftplistparser.c getenv.c \
-	getinfo.c gopher.c hash.c hmac.c hostasyn.c hostcheck.c hostip.c hostip4.c \
-	hostip6.c hostsyn.c http_chunks.c http_digest.c http_negotiate.c http_ntlm.c \
-	http_proxy.c http.c http2.c idn_win32.c if2ip.c imap.c inet_ntop.c \
-	inet_pton.c krb5.c ldap.c llist.c md4.c md5.c memdebug.c mime.c mprintf.c \
-	multi.c netrc.c non-ascii.c nonblock.c nwlib.c nwos.c openldap.c parsedate.c \
-	pingpong.c pipeline.c pop3.c progress.c rand.c rtsp.c security.c select.c \
-	sendf.c setopt.c sha256.c share.c slist.c smb.c smtp.c socks.c socks_gssapi.c \
-	socks_sspi.c speedcheck.c splay.c ssh.c ssh-libssh.c strcase.c strdup.c \
-	strerror.c strtok.c strtoofft.c system_win32.c telnet.c tftp.c timeval.c \
-	transfer.c url.c version.c warnless.c wildcard.c x509asn1.c vtls/axtls.c \
-	vtls/cyassl.c vtls/darwinssl.c vtls/gskit.c vtls/gtls.c vtls/mbedtls.c \
-	vtls/nss.c vtls/openssl.c vtls/polarssl_threadlock.c vtls/polarssl.c \
-	vtls/schannel_verify.c vtls/schannel.c vtls/vtls.c vauth/cleartext.c \
-	vauth/cram.c vauth/digest.c vauth/digest_sspi.c vauth/krb5_gssapi.c \
-	vauth/krb5_sspi.c vauth/ntlm_sspi.c vauth/ntlm.c vauth/oauth2.c \
-	vauth/spnego_gssapi.c vauth/spnego_sspi.c vauth/vauth.c
+  strcase.c dict.c llist.c mprintf.c pingpong.c socks_gssapi.c psl.c url.c \
+  timeval.c curl_get_line.c idn_win32.c hmac.c md4.c curl_range.c hostsyn.c \
+  strtok.c curl_threads.c if2ip.c http_negotiate.c nwlib.c doh.c curl_endian.c \
+  formdata.c hostcheck.c rand.c http2.c content_encoding.c hostip.c escape.c \
+  easy.c share.c slist.c inet_pton.c tftp.c socks.c parsedate.c warnless.c \
+  nwos.c system_win32.c transfer.c curl_rtmp.c nonblock.c select.c hostip4.c \
+  urlapi.c openldap.c getenv.c hash.c http_proxy.c vquic/quiche.c vquic/ngtcp2.c \
+  krb5.c multi.c strdup.c mime.c socks_sspi.c smtp.c curl_addrinfo.c memdebug.c \
+  progress.c curl_ntlm_core.c curl_path.c hostasyn.c pop3.c gopher.c rtsp.c \
+  curl_ntlm_wb.c curl_gethostname.c curl_des.c base64.c splay.c curl_ctype.c \
+  dotdot.c http.c curl_sspi.c http_chunks.c telnet.c amigaos.c fileinfo.c \
+  version.c ldap.c curl_sasl.c netrc.c socketpair.c strerror.c curl_multibyte.c \
+  altsvc.c conncache.c curl_memrchr.c smb.c sha256.c connect.c ftp.c strtoofft.c \
+  md5.c vauth/krb5_sspi.c vauth/spnego_sspi.c vauth/ntlm.c vauth/spnego_gssapi.c \
+  vauth/ntlm_sspi.c vauth/vauth.c vauth/oauth2.c vauth/cram.c vauth/cleartext.c \
+  vauth/krb5_gssapi.c vauth/digest.c vauth/digest_sspi.c file.c http_digest.c \
+  asyn-thread.c vtls/gskit.c vtls/mbedtls.c vtls/gtls.c vtls/bearssl.c \
+  vtls/polarssl.c vtls/schannel.c vtls/sectransp.c vtls/schannel_verify.c \
+  vtls/vtls.c vtls/polarssl_threadlock.c vtls/mesalink.c vtls/nss.c \
+  vtls/openssl.c vtls/wolfssl.c vssh/libssh.c vssh/libssh2.c asyn-ares.c imap.c \
+  security.c cookie.c hostip6.c sendf.c ftplistparser.c http_ntlm.c x509asn1.c \
+  setopt.c non-ascii.c getinfo.c speedcheck.c inet_ntop.c wildcard.c curl_gssapi.c \
+  curl_fnmatch.c 
 CURL_LOCAL_SRC_FILES := $(addprefix ../../curl/lib/,$(CURL_CSOURCES))
 CURL_LOCAL_C_INCLUDES += \
   $(LOCAL_PATH)/../../curl/include \
